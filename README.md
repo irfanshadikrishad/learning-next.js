3536a8232f51763d849c618a39b9e419f3dcd9a3
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2023-11-19 11:09:45 +0600

    optimizing/images

--- README.md
+++ README.md
@@ -73,3 +73,4 @@
 #### CRUD => Create, Read, Update & Delete
 
 - ~~api/crud/1~~ `44a755c`
+- ~~api/crud/2~~ `981ac74`
--- app/page.js
+++ app/page.js
@@ -1,7 +1,26 @@
+import Image from "next/image";
+import cat from "@/public/cat.jpg";
+
 export default function Homemade_Apple() {
   return (
     <div className="containor">
       <h1>Home</h1>
+      <div className="image">
+        <Image
+          src={cat}
+          // src="https://i.pinimg.com/564x/30/a1/d9/30a1d94c1e73e791f364c150c5471ce4.jpg"
+          alt="cat"
+          // Make the image display full width
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          width={500}
+          height={500}
+          placeholder="blur"
+          priority="true"
+        />
+      </div>
     </div>
   );
 }
